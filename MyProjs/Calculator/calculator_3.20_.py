9afe20f75e3f81bbfc64b17125de3e8cb00bccc1
Royal_Aeo <Aeo_royals@outlook.com>
2022-01-12 21:41:53 +0530

    pep changes

--- MyProjs/Calculator/calculator_3.20_.py
+++ MyProjs/Calculator/calculator_3.20_.py
@@ -1,33 +1,38 @@
-﻿#actually, system unable to recorgnise user_input
-user_input ="yes"
-print("CALCULATOR_2.0_\n","General instruction\n",">>1. You don't have to give input in shortform/longform\n",">>2. You only have to give input according to instruction you gave before giving input\n",">>3. if you give input in wrong way the program could be stop running\n",">>4. instruction will given in a bracket with '~'\n",">>5. warning is written in square bracket '[]' with '~'\n")
-#whenever user is want to calculate numbers it will continue
+﻿# actually, system unable to recorgnise user_input
+user_input = "yes"
+print("CALCULATOR_2.0_\n", "General instruction\n",
+      ">>1. You don't have to give input in shortform/expanded form\n",
+      ">>2. You only have to give input according to instruction you gave before giving input\n",
+      ">>3. if you give input in wrong way the program could be stop running\n",
+      ">>4. instruction will given in a bracket with '~'\n",
+      ">>5. warning is written in square bracket '[]' with '~'\n")
+# whenever user is want to calculate numbers it will continue
 while user_input != "no":
-#Collecting data from user(input)
-    user_want = input("What you want to do? calacuate two numbers or find some values of any number(~Only enter 'calculate' for calculating two numbers or enter 'findsomeval' for find some values of any number)\n")
+    # Collecting data from user(input)
+    user_want = input("What you want to do? (~calculate/findsomeval\n")
     if user_want == "calculate":
-        #for first number
+        # for first number
         firstnum = float(input("Enter first number (~Only enter number not alphabet)\n"))
 
-       #for second number
+        # for second number
         secondnum = float(input("Enter second number (~Only enter number not alphabet)\n"))
 
-       #for operator
-        operaor = input("Enter operator, options:- +,-,*,/ or plus,minus,multiply,divide (~Only enter symbol that given in the option not any other thing)\n")
+        # for operator
+        operaor = input("Enter operator, options:- +,-,*,/ or plus,minus,multiply,divide\n")
         
-        #for plus
+        # for plus
         if operaor == "+" or operaor == "plus":
             print(firstnum + secondnum)
 
-        #for minus
+        # for minus
         elif operaor == "-" or operaor == "minus":
             print(firstnum - secondnum)
 
-        #for divide
+        # for divide
         elif operaor == "/" or operaor == "divide":
             print(firstnum / secondnum)
 
-        #for multiply
+        # for multiply
         elif operaor == "*" or operaor == "multiply" or operaor == "×" or operaor == "X":
             print(firstnum * secondnum)
     elif user_want == "findsomeval":
@@ -39,20 +44,16 @@ while user_input != "no":
         elif operaor == "%" or operaor == "reminder":
             divby = float(input("Enter number which you want to divide by(~Only enter number not more)\n"))
             print(num % divby)
-   #if any of the condision is not same then:-
+    # if any of the condision is not same then:-
    
-    #in this type of case usually problem present in data that giver by user(user give data in wrong way)
+    # in this type of case usually problem present in data that giver by user(user give data in wrong way)
     else:
         print("Any thing went wrong")
 
-        #It is all ready give a warning to user for they not do any mistake in giving input
+        # It is all ready give a warning to user for they not do any mistake in giving input
     print("warning[~ if you enter wrong input program will stop]")
 
-    #if user want to calculate any two number again it will continue
+    # if user want to calculate any two number again it will continue
     user_input = input("Do you want to try again? yes/no (~Only enter 'yes' or 'no'} \n")
     if user_input != "yes" and user_input != "no":
         user_input = "no"
-    
-    
-
-

c36a1054271a7fbfca02753d7eecfae61da2cc29
ROYAL_AEO <80609498+Royals-Aeo-Gamer@users.noreply.github.com>
2022-01-16 15:25:00 +0530

    .

--- MyProjs/Tic-tac-toe/tic-tac-toe.py
+++ MyProjs/Tic-tac-toe/tic-tac-toe.py
@@ -1,1 +1,19 @@
 # all global varables
+avalabel_sign = ["X","Y","A","B","P","Z","S"]
+
+var =f"""
+      |      |
+   {1}  |  {2}   |  {3}  
+______|______|______
+      |      |      
+   {2}  |  {3}   |  {3}  
+______|______|______
+      |      |      
+   {3}  |  {4}   |  {5}  
+      |      |      
+"""
+class table:
+    def _prin():
+        print("hi")
+
+table._prin()
